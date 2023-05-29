162e50fee5bedf7e7817df2bebd90d8a5fb075ae
Maxim Golikov <frystration@gmail.com>
2023-05-29 11:03:12 +0300

    deploy
    deploy
    
    deploy
    
    deploy

--- src/hooks/useTextFetching.ts
+++ src/hooks/useTextFetching.ts
@@ -2,22 +2,38 @@ import {Ref, ref, watch} from "vue";
 import axios from "axios";
 import fixFetchedText from "../utils/fixFetchedText.ts";
 import {Language, LanguageValue} from "../types";
+import jsonData from '../API/package.json';
 
 export const useTextFetching = () => {
     const selectedLanguage: Ref<LanguageValue | ""> = ref("");
     const text: Ref<string> = ref("");
     const isLoading: Ref<boolean> = ref(true);
 
+    // const fetchingMock = async () => {
+    //     try {
+    //         const response = await axios.get('/src/API/package.json')
+    //         text.value = fixFetchedText(response.data.text);
+    //     } catch (e) {
+    //         alert("Ошибка API");
+    //     } finally {
+    //         setTimeout(() => {
+    //             isLoading.value = false;
+    //         }, 500)
+    //     }
+    // };
     const fetchingMock = async () => {
         try {
-            const response = await axios.get('http://localhost:5173/src/API/package.json')
-            text.value = fixFetchedText(response.data.text);
+            const getDataFromServer = async ():Promise<{text: string}> => {
+                return await new Promise((resolve) => {
+                    setTimeout(() => resolve(jsonData), 1000);
+                });
+            };
+            const response = await getDataFromServer()
+            text.value = fixFetchedText(response.text);
         } catch (e) {
             alert("Ошибка API");
         } finally {
-            setTimeout(() => {
-                isLoading.value = false;
-            }, 500)
+            isLoading.value = false;
         }
     };
 
